2285995a34cd1e94782f0aa3a0cb208470038f29
karstenlenz <mr.karsten.lenz@gmail.com>
2020-08-29 21:05:42 +0200

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Questionnaire from './Questionnaire/Questionnaire'
-
 function App() {
   return (
-    <>
+    <main>
       <Questionnaire />
-    </>
+    </main>
   )
 }
 
---
+++ src/GlobalStyles.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+* {
+    box-sizing:border-box;
+}
+
+body {
+    font-size:112.5%;
+    margin:0 auto;
+    max-width:600px;
+}
+
+main {
+    padding:15px;
+}
+
+
+h1 {
+    font-size:2.5em;
+}
+
+h2 {
+    font-size:2em;
+}
+
+h3 {
+    font-size:1.725em;
+}
+
+h4 {
+    font-size:1.45em;
+}
+
+h5 {
+    font-size: 1.2em;
+}
+
+h6 {
+    font-size:1em;
+}
+
+
+`
