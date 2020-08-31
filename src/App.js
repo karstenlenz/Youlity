0c591618c39a9c3fd421850fb02ffd9cb4ec0ffc
karstenlenz <mr.karsten.lenz@gmail.com>
2020-08-31 11:23:22 +0200

    Install prop-types

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Questionnaire from './Questionnaire/Questionnaire'
 import { Switch, Route } from 'react-router-dom'
-import Motives from './Motives'
+import Motives from './Motives/Motives'
 function App() {
+  const [userMotives, setUserMotives] = useState([])
+
   return (
     <main>
       <Switch>
-        <Route path="/motives">
-          <Motives />
-        </Route>
         <Route path="/">
+          <Motives userMotives={userMotives} setUserMotives={setUserMotives} />
+        </Route>
+        <Route path="/questionnaire">
           <Questionnaire />
         </Route>
       </Switch>
