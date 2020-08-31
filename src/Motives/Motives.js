55693493d47c8979f88f87ddfb74e66c17d2a3f5
karstenlenz <mr.karsten.lenz@gmail.com>
2020-08-31 12:50:27 +0200

    Add selection for motives

--- src/Motives/Motives.js
+++ src/Motives/Motives.js
@@ -7,5 +7,45 @@ Motives.propTypes = {
 }
 
 export default function Motives({ userMotives, setUserMotives }) {
-  return <p>Motives page will be added here</p>
+  return (
+    <>
+      <h1>Bedürfnisse</h1>
+      <h2>
+        Bitte wählen Sie die 3 Bedürfnisse aus, die Ihnen am wichtigsten sind.
+        Fangen Sie mit dem wichtigsten an.
+      </h2>
+      <ol>
+        <li> {userMotives[0]} </li>
+        <li> {userMotives[1]} </li>
+        <li> {userMotives[2]} </li>
+      </ol>
+
+      <section>
+        <button onClick={(event) => handleClick(event, 'Anerkennung')}>
+          Anerkennung
+        </button>
+        <button onClick={(event) => handleClick(event, 'Wichtigkeit')}>
+          Wichtigkeit
+        </button>
+        <button onClick={(event) => handleClick(event, 'Verlässlichkeit')}>
+          Verlässlichkeit
+        </button>
+        <button onClick={(event) => handleClick(event, 'Solidarität')}>
+          Solidarität
+        </button>
+        <button onClick={(event) => handleClick(event, 'Autonomie')}>
+          Autonomie
+        </button>
+        <button onClick={(event) => handleClick(event, 'Grenzen')}>
+          Grenzen
+        </button>
+      </section>
+    </>
+  )
+
+  function handleClick(event, motive) {
+    const button = event.target
+    button.disabled = true
+    setUserMotives([...userMotives, motive])
+  }
 }
