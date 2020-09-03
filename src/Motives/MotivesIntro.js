2a0252e755744b85006ad9cf546ed25bbc6c039f
karstenlenz <mr.karsten.lenz@gmail.com>
2020-09-03 16:20:28 +0200

    Add component MotivesIntro

--- src/Motives/MotivesIntro.js
+++ src/Motives/MotivesIntro.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import Button from '../common/Button'
+import { Link } from 'react-router-dom'
 
 export default function MotivesIntro() {
-  return <></>
+  return (
+    <>
+      <h1>Schritt 1: Bedürfnisse</h1>
+      <img alt="" src="/img/motives-intro.svg" />
+      <p>
+        Die Persönlichkeit jedes Mensch ist von 6 Grundbedürfnissen bestimmt.
+        Dabei gibt es bei jedem von uns eine unterschiedliche Gewichtung. In
+        Schritt 1 müssen Sie für sich die wichtigsten drei Themen auswählen, die
+        Sie in Ihrem Leben und Ihren Beziehungen zu anderen Menschen am meisten
+        beschäftigen. <br />
+        Diese Grundbedürfnisse lassen erste Rückschlüsse auf Ihre
+        Persönlichkeitsstile zu. In Schritt 2 werden Sie dann 2 Fragebögen
+        ausfüllen, um konkret zu prüfen, wie ausgeprägt diese Stile bei Ihnen
+        sind.
+      </p>
+      <Link to="/motives/selection">
+        <Button>Weiter zur Motivauswahl</Button>
+      </Link>
+    </>
+  )
 }
---
+++ src/Motives/MotivesIntro.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MotivesIntro from './MotivesIntro'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+export default {
+  title: 'Motives Intro',
+  component: MotivesIntro,
+}
+
+export const MotivesIntroComponent = () => (
+  <Router>
+    <MotivesIntro />
+  </Router>
+)
