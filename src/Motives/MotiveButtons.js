e346ed1ac32173629a6fbacc17412d9b14f4864b
karstenlenz <mr.karsten.lenz@gmail.com>
2020-09-04 10:42:55 +0200

    Update motiveData so that it returns a JSX component for the overlay content

--- src/Motives/MotiveButtons.js
+++ src/Motives/MotiveButtons.js
@@ -7,79 +7,118 @@ export default function MotiveButtons({ handleMotiveClick }) {
   const motiveData = [
     {
       name: 'Anerkennung',
-      description: `<p>
-    Das Motiv nach Anerkennung bedeutet, dass man von anderen
-    Personen die Botschaft erhalten möchte, dass man als Person o.k.
-    und liebenswert sowie kompetent ist, über Fähigkeiten oder über
-    Attraktivität verfügt und insgesamt positive Eigenschaften
-    aufweist. Man möchte z. B. hören:
-  </p>
-  <ul>
-    <li>„Du bist als Person o. k.“</li>
-    <li>„Du bist intelligent.“</li>
-    <li>„Du siehst gut aus.“</li>
-    <li>„Du bist attraktiv.“</li>
-  </ul>`,
+      description: () => {
+        return (
+          <>
+            <p>
+              Das Bedürfnis nach Anerkennung bedeutet, Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Ipsum sit quod accusantium
+              voluptatibus tenetur dolor? Necessitatibus, debitis unde
+              repellendus non cupiditate doloribus? Accusantium debitis
+              temporibus vel aut, consectetur distinctio! Obcaecati eum vel
+              natus mollitia voluptatibus ullam quae esse sit tempora.
+            </p>
+            <ul>
+              <li>„Du bist als lorem“</li>
+              <li>„Du bist ipsum.“</li>
+              <li>„Du siehst dolor aus.“</li>
+              <li>„Du bist sit.“</li>
+            </ul>
+          </>
+        )
+      },
     },
     {
       name: 'Wichtigkeit',
-      description: `<p>
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eius ullam beatae dolor ipsum ut quis magnam iusto alias reiciendis suscipit illum commodi reprehenderit. Maxime numquam molestias, consequatur eos accusamus esse est consequuntur aut! Laudantium eligendi modi dolore soluta autem optio! Nisi dolor exercitationem beatae velit nam quisquam numquam earum voluptates enim, et totam quaerat, esse dolore. Quaerat cupiditate quidem doloribus aspernatur, molestiae nemo nisi distinctio dignissimos possimus, autem tenetur natus recusandae architecto. Dignissimos iste, ab quo cumque provident labore ad harum quam cum error saepe quidem magni ipsam laborum ipsa tempora, dolores eos accusamus consectetur quasi reiciendis suscipit soluta!
-</p>
-<ul>
-  <li>„Du bist als Person lorem ipsum.“</li>
-  <li>„Du bist lorem ipsum.“</li>
-  <li>„Du siehst lorem ipsum aus.“</li>
-  <li>„Du bist lorem ipsum.“</li>
-</ul>`,
-    },
-    {
-      name: 'Verlässlichkeit',
-      description: `<p>
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eius ullam beatae dolor ipsum ut quis magnam iusto alias reiciendis suscipit illum commodi reprehenderit. Maxime numquam molestias, consequatur eos accusamus esse est consequuntur aut! Laudantium eligendi modi dolore soluta autem optio! Nisi dolor exercitationem beatae velit nam quisquam numquam earum voluptates enim, et totam quaerat, esse dolore. Quaerat cupiditate quidem doloribus aspernatur, molestiae nemo nisi distinctio dignissimos possimus, autem tenetur natus recusandae architecto. Dignissimos iste, ab quo cumque provident labore ad harum quam cum error saepe quidem magni ipsam laborum ipsa tempora, dolores eos accusamus consectetur quasi reiciendis suscipit soluta!
-</p>
-<ul>
-<li>„Du bist als Person lorem ipsum.“</li>
-<li>„Du bist lorem ipsum.“</li>
-<li>„Du siehst lorem ipsum aus.“</li>
-<li>„Du bist lorem ipsum.“</li>
-</ul>`,
+      description: () => {
+        return (
+          <>
+            <p>
+              Das Bedürfnis nach Wichtigkeit bedeutet, Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Ipsum sit quod accusantium
+              voluptatibus tenetur dolor? Necessitatibus, debitis unde
+              repellendus non cupiditate doloribus? Accusantium debitis
+              temporibus vel aut, consectetur distinctio! Obcaecati eum vel
+              natus mollitia voluptatibus ullam quae esse sit tempora.
+            </p>
+            <ul>
+              <li>„Du bist als lorem“</li>
+              <li>„Du bist ipsum.“</li>
+              <li>„Du siehst dolor aus.“</li>
+              <li>„Du bist sit.“</li>
+            </ul>
+          </>
+        )
+      },
     },
     {
       name: 'Solidarität',
-      description: `<p>
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eius ullam beatae dolor ipsum ut quis magnam iusto alias reiciendis suscipit illum commodi reprehenderit. Maxime numquam molestias, consequatur eos accusamus esse est consequuntur aut! Laudantium eligendi modi dolore soluta autem optio! Nisi dolor exercitationem beatae velit nam quisquam numquam earum voluptates enim, et totam quaerat, esse dolore. Quaerat cupiditate quidem doloribus aspernatur, molestiae nemo nisi distinctio dignissimos possimus, autem tenetur natus recusandae architecto. Dignissimos iste, ab quo cumque provident labore ad harum quam cum error saepe quidem magni ipsam laborum ipsa tempora, dolores eos accusamus consectetur quasi reiciendis suscipit soluta!
-</p>
-<ul>
-<li>„Du bist als Person lorem ipsum.“</li>
-<li>„Du bist lorem ipsum.“</li>
-<li>„Du siehst lorem ipsum aus.“</li>
-<li>„Du bist lorem ipsum.“</li>
-</ul>`,
+      description: () => {
+        return (
+          <>
+            <p>
+              Das Bedürfnis nach Solidarität bedeutet, Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Ipsum sit quod accusantium
+              voluptatibus tenetur dolor? Necessitatibus, debitis unde
+              repellendus non cupiditate doloribus? Accusantium debitis
+              temporibus vel aut, consectetur distinctio! Obcaecati eum vel
+              natus mollitia voluptatibus ullam quae esse sit tempora.
+            </p>
+            <ul>
+              <li>„Du bist als lorem“</li>
+              <li>„Du bist ipsum.“</li>
+              <li>„Du siehst dolor aus.“</li>
+              <li>„Du bist sit.“</li>
+            </ul>
+          </>
+        )
+      },
     },
     {
       name: 'Autonomie',
-      description: `<p>
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eius ullam beatae dolor ipsum ut quis magnam iusto alias reiciendis suscipit illum commodi reprehenderit. Maxime numquam molestias, consequatur eos accusamus esse est consequuntur aut! Laudantium eligendi modi dolore soluta autem optio! Nisi dolor exercitationem beatae velit nam quisquam numquam earum voluptates enim, et totam quaerat, esse dolore. Quaerat cupiditate quidem doloribus aspernatur, molestiae nemo nisi distinctio dignissimos possimus, autem tenetur natus recusandae architecto. Dignissimos iste, ab quo cumque provident labore ad harum quam cum error saepe quidem magni ipsam laborum ipsa tempora, dolores eos accusamus consectetur quasi reiciendis suscipit soluta!
-</p>
-<ul>
-<li>„Du bist als Person lorem ipsum.“</li>
-<li>„Du bist lorem ipsum.“</li>
-<li>„Du siehst lorem ipsum aus.“</li>
-<li>„Du bist lorem ipsum.“</li>
-</ul>`,
+      description: () => {
+        return (
+          <>
+            <p>
+              Das Bedürfnis nach Autonomie bedeutet, Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Ipsum sit quod accusantium
+              voluptatibus tenetur dolor? Necessitatibus, debitis unde
+              repellendus non cupiditate doloribus? Accusantium debitis
+              temporibus vel aut, consectetur distinctio! Obcaecati eum vel
+              natus mollitia voluptatibus ullam quae esse sit tempora.
+            </p>
+            <ul>
+              <li>„Du bist als lorem“</li>
+              <li>„Du bist ipsum.“</li>
+              <li>„Du siehst dolor aus.“</li>
+              <li>„Du bist sit.“</li>
+            </ul>
+          </>
+        )
+      },
     },
     {
       name: 'Grenzen',
-      description: `<p>
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eius ullam beatae dolor ipsum ut quis magnam iusto alias reiciendis suscipit illum commodi reprehenderit. Maxime numquam molestias, consequatur eos accusamus esse est consequuntur aut! Laudantium eligendi modi dolore soluta autem optio! Nisi dolor exercitationem beatae velit nam quisquam numquam earum voluptates enim, et totam quaerat, esse dolore. Quaerat cupiditate quidem doloribus aspernatur, molestiae nemo nisi distinctio dignissimos possimus, autem tenetur natus recusandae architecto. Dignissimos iste, ab quo cumque provident labore ad harum quam cum error saepe quidem magni ipsam laborum ipsa tempora, dolores eos accusamus consectetur quasi reiciendis suscipit soluta!
-</p>
-<ul>
-<li>„Du bist als Person lorem ipsum.“</li>
-<li>„Du bist lorem ipsum.“</li>
-<li>„Du siehst lorem ipsum aus.“</li>
-<li>„Du bist lorem ipsum.“</li>
-</ul>`,
+      description: () => {
+        return (
+          <>
+            <p>
+              Das Bedürfnis nach Grenzen bedeutet, Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Ipsum sit quod accusantium
+              voluptatibus tenetur dolor? Necessitatibus, debitis unde
+              repellendus non cupiditate doloribus? Accusantium debitis
+              temporibus vel aut, consectetur distinctio! Obcaecati eum vel
+              natus mollitia voluptatibus ullam quae esse sit tempora.
+            </p>
+            <ul>
+              <li>„Du bist als lorem“</li>
+              <li>„Du bist ipsum.“</li>
+              <li>„Du siehst dolor aus.“</li>
+              <li>„Du bist sit.“</li>
+            </ul>
+          </>
+        )
+      },
     },
   ]
 
@@ -94,7 +133,7 @@ Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eius ullam beata
             >
               {motive.name}
             </Button>
-            <InfoOverlay>{motive.description}</InfoOverlay>
+            <InfoOverlay>{motive.description()}</InfoOverlay>
           </>
         )
       })}
