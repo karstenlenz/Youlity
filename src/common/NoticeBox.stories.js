52952fae37225099fe3cf0db1e0297eefd23aa0a
karstenlenz <mr.karsten.lenz@gmail.com>
2020-09-07 22:04:07 +0200

    Add missing components to Storybook

---
+++ src/common/NoticeBox.stories.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import NoticeBox from './NoticeBox'
+import TextLink from './TextLink'
+
+export default {
+  title: 'NoticeBox',
+  component: NoticeBox,
+}
+
+export const ExampleNoticeBox = (args) => (
+  <NoticeBox {...args}>
+    <h3>Hinweis</h3>
+    <p>
+      Unser Testergebnis stellt nur eine Tendenz dar und ersetzt keine
+      psychologische Beratung. Wenn Sie sich unwohl fühlen, holen Sie sich
+      professionelle Hilfe.
+    </p>
+    <TextLink href="https://www.psychenet.de/de/hilfe-finden/schnelle-hilfe/krisenanlaufstellen.html">
+      Ansprechpartner &gt;
+    </TextLink>
+  </NoticeBox>
+)
