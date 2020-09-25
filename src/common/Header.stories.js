6e92bc9eb18a74797e23b079d954e710daa63f15
karstenlenz <mr.karsten.lenz@gmail.com>
2020-09-25 16:49:06 +0200

    Update Storybook

--- src/common/Header.stories.js
+++ src/common/Header.stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './Header'
 
 export default {
@@ -6,4 +7,8 @@ export default {
   component: Header,
 }
 
-export const HeaderExample = () => <Header />
+export const HeaderExample = () => (
+  <Router>
+    <Header />
+  </Router>
+)
