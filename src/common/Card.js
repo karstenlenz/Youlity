385243408b967bba66b8353f9d485df64b79cd95
karstenlenz <mr.karsten.lenz@gmail.com>
2020-09-28 12:04:42 +0200

    Add breakpoint function to util.js

--- src/common/Card.js
+++ src/common/Card.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import styled from 'styled-components/macro'
+import { breakpoints } from './util'
 
 export default function Card({ children }) {
   return <CardBG>{children}</CardBG>
 }
 
 const CardBG = styled.div`
+  display: inline-block;
   background: white;
   border-radius: 12px;
   box-shadow: var(--primary-shadow);
   margin: 30px 0;
   padding: 15px;
   position: relative;
+  ${breakpoints('max-width', '%', [{ 750: 40 }])}
 `
--- src/common/util.js
+++ src/common/util.js
@@ -1,3 +1,25 @@
+import { css } from 'styled-components'
+
+export const breakpoints = (
+  prop = 'padding', // the CSS property to apply to the breakpoints
+  propUnit = 'px', // the units of the CSS property (can set equal to "" and apply units to values directly)
+  values = [], // array of objects, e.g. [{ 800: 60 }, ...] <-- 800 (key) = screen breakpoint, 60 (value) = CSS prop breakpoint
+  mediaQueryType = 'min-width' // media query breakpoint type, i.e.: max-width, min-width, max-height, min-height
+) => {
+  const breakpointProps = values.reduce((mediaQueries, value) => {
+    const [screenBreakpoint, cssPropBreakpoint] = [
+      Object.keys(value)[0],
+      Object.values(value)[0],
+    ]
+    return (mediaQueries += `
+    @media screen and (${mediaQueryType}: ${screenBreakpoint}px) {
+      ${prop}: ${cssPropBreakpoint}${propUnit};
+    }
+    `)
+  }, '')
+  return css([breakpointProps])
+}
+
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
